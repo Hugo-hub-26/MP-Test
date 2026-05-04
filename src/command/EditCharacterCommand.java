55aae6db3ed7cf74d7c5e4008f72614fd22f1dc1
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-04 18:05:27 +0200

    "Avances"

--- src/command/EditCharacterCommand.java
+++ src/command/EditCharacterCommand.java
@@ -4,15 +4,45 @@
  */
 package command;
 
+import control.GameContext;
+import domain.Armor;
+import domain.Discipline;
+import domain.Gift;
+import domain.Strength;
+import domain.Weakness;
+import domain.Weapons;
+import domain.Will;
+import java.util.HashMap;
+
 /**
  *
  * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
  */
 public class EditCharacterCommand implements Command{
+    private final GameContext context;
+    private HashMap<String,Discipline> discipline;
+    private HashMap<String,Gift> gift;
+    private HashMap<String,Will> will;
+    private HashMap<String,Armor> armor; 
+    private HashMap<String,Weapons> weapon; 
+    private HashMap<String,Strength> strength; 
+    private HashMap<String,Weakness> weakness; 
 
-	@Override
-	public void execute() {
-		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-	}
+    public EditCharacterCommand(GameContext context) {
+        this.context = context;
+        this.discipline = context.getCatalog().getDiscipline();
+        this.gift = context.getCatalog().getGift();
+        this.will = context.getCatalog().getWill();
+        this.armor = context.getCatalog().getArmor();
+        this.weapon = context.getCatalog().getWeapon();
+        this.strength = context.getCatalog().getStrength();
+        this.weakness = context.getCatalog().getWeakness();
+    }
 	
+    @Override
+	public void execute() {
+                        
+        
+        
+        }
 }
