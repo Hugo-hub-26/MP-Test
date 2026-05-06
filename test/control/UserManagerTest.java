768dc80254ca3372b885e90966cae4aba0845359
MikePradiBart <m.pradillo.2020@alumnos.urjc.es>
2026-05-06 18:42:12 +0200

    UserManagerTest

--- test/control/UserManagerTest.java
+++ test/control/UserManagerTest.java
@@ -4,6 +4,7 @@
  */
 package control;
 
+import domain.Player;
 import domain.User;
 import java.util.Collection;
 import java.util.Map;
@@ -16,120 +17,143 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
+ * @author Miguel Pradillo Bartolomé
  */
 public class UserManagerTest {
-	
-	public UserManagerTest() {
-	}
-	
-	@BeforeClass
-	public static void setUpClass() {
-	}
-	
-	@AfterClass
-	public static void tearDownClass() {
-	}
-	
-	@Before
-	public void setUp() {
-	}
-	
-	@After
-	public void tearDown() {
-	}
+    
+    private UserManager instance;
+    private Player testUser;
+    
+    public UserManagerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        instance = new UserManager();
+        testUser = new Player("Miguel", "miguel_test", "pass123", null);
+    }
+    
+    @After
+    public void tearDown() {
+        instance = null;
+        testUser = null;
+    }
 
-	/**
-	 * Test of load method, of class UserManager.
-	 */
-	@Test
-	public void testLoad() {
-		System.out.println("load");
-		UserManager instance = new UserManager();
-		instance.load();
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of load method, of class UserManager.
+     */
+    @Test
+    public void testLoad() {
+        System.out.println("load");
+        try {
+            instance.load();
+            assertTrue("El método load se ha ejecutado sin excepciones", true);
+        } catch (Exception e) {
+            fail("El método load ha lanzado una excepción: " + e.getMessage());
+        }
+    }
 
-	/**
-	 * Test of save method, of class UserManager.
-	 */
-	@Test
-	public void testSave() {
-		System.out.println("save");
-		UserManager instance = new UserManager();
-		instance.save();
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of save method, of class UserManager.
+     */
+    @Test
+    public void testSave() {
+        System.out.println("save");
+        try {
+            instance.add(testUser);
+            instance.save();
+            assertTrue("El método save se ha ejecutado sin excepciones", true);
+        } catch (Exception e) {
+            fail("El método save ha lanzado una excepción: " + e.getMessage());
+        }
+    }
 
-	/**
-	 * Test of findByNick method, of class UserManager.
-	 */
-	@Test
-	public void testFindByNick() {
-		System.out.println("findByNick");
-		String nick = "";
-		UserManager instance = new UserManager();
-		User expResult = null;
-		User result = instance.findByNick(nick);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of findByNick method, of class UserManager.
+     */
+    @Test
+    public void testFindByNick() {
+        System.out.println("findByNick");
+        instance.add(testUser);
+        
+        String nick = "miguel_test";
+        User expResult = testUser;
+        User result = instance.findByNick(nick);
+        
+        // Verificamos que devuelve el usuario correcto
+        assertEquals("Debería encontrar al usuario recién insertado", expResult, result);
+        
+        // Verificamos el comportamiento al buscar un nick que no existe
+        User nullResult = instance.findByNick("usuario_inventado");
+        assertNull("Debería devolver null si el nick no existe", nullResult);
+    }
 
-	/**
-	 * Test of add method, of class UserManager.
-	 */
-	@Test
-	public void testAdd() {
-		System.out.println("add");
-		User u = null;
-		UserManager instance = new UserManager();
-		instance.add(u);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of add method, of class UserManager.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        
+        instance.add(testUser);
+        
+        User result = instance.findByNick("miguel_test");
+        assertNotNull("El usuario no debería ser null tras añadirlo", result);
+        assertEquals("La instancia recuperada debe ser la misma que la añadida", testUser, result);
+    }
 
-	/**
-	 * Test of remove method, of class UserManager.
-	 */
-	@Test
-	public void testRemove() {
-		System.out.println("remove");
-		String nick = "";
-		UserManager instance = new UserManager();
-		instance.remove(nick);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of remove method, of class UserManager.
+     */
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        
+        instance.add(testUser);
+        assertNotNull(instance.findByNick("miguel_test"));
+        
+        String nick = "miguel_test";
+        instance.remove(nick);
+        
+        User result = instance.findByNick(nick);
+        assertNull("El usuario debería haber sido eliminado del sistema", result);
+    }
 
-	/**
-	 * Test of getUsers method, of class UserManager.
-	 */
-	@Test
-	public void testGetUsers() {
-		System.out.println("getUsers");
-		UserManager instance = new UserManager();
-		Collection<User> expResult = null;
-		Collection<User> result = instance.getUsers();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    /**
+     * Test of getUsers method, of class UserManager.
+     */
+    @Test
+    public void testGetUsers() {
+        System.out.println("getUsers");
+        
+        instance.add(testUser);
+        
+        Collection<User> result = instance.getUsers();
+        assertNotNull("La colección devuelta no debe ser nula", result);
+        assertTrue("La colección debe contener al usuario de prueba", result.contains(testUser));
+    }
 
-	/**
-	 * Test of getUsuarios method, of class UserManager.
-	 */
-	@Test
-	public void testGetUsuarios() {
-		System.out.println("getUsuarios");
-		UserManager instance = new UserManager();
-		Map<String, User> expResult = null;
-		Map<String, User> result = instance.getUsuarios();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-	
+    /**
+     * Test of getUsuarios method, of class UserManager.
+     */
+    @Test
+    public void testGetUsuarios() {
+        System.out.println("getUsuarios");
+        
+        instance.add(testUser);
+        
+        Map<String, User> result = instance.getUsuarios();
+        assertNotNull("El mapa devuelto no debe ser nulo", result);
+        assertTrue("El mapa debe contener la clave del nick del usuario", result.containsKey("miguel_test"));
+        assertEquals("El valor mapeado debe corresponder al usuario insertado", testUser, result.get("miguel_test"));
+    }
+    
 }
