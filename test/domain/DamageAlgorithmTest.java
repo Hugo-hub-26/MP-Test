2a80fb4711151582a94af7d13ce398d779b45bc6
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-05-06 21:00:36 +0200

    Merge

--- test/domain/DamageAlgorithmTest.java
+++ test/domain/DamageAlgorithmTest.java
@@ -33,12 +33,8 @@ public class DamageAlgorithmTest {
     
     @Before
     public void setUp() {
-<<<<<<< HEAD
         // Inicializamos el algoritmo antes de cada prueba
         instance = new DamageAlgorithm(1);
-=======
-        instance = new DamageAlgorithm();
->>>>>>> e396613e456f62282c76584337cdc7eaa7ab3bc2
     }
     
     @After
@@ -53,7 +49,6 @@ public class DamageAlgorithmTest {
     public void testExecute_BasePowerOnly() {
         System.out.println("execute - Solo Poder Base");
         
-<<<<<<< HEAD
 		HashMap<String, Gift> abilities = new HashMap<>();
         abilities.put("Fuego", new Gift("Fuego", "Muy duro", 2, 3));
         abilities.put("Fuegosss", new Gift("Fuegosss", "Muy duro", 2, 3));
@@ -76,17 +71,6 @@ public class DamageAlgorithmTest {
         
         GameCharacter c = new Hunter();
 		c = new HunterCharacterBuilder(abilities, armors, weapons, strengths, weaknesses).gameCharacterBuilder(c);
-=======
-        // Instanciamos la habilidad (Voluntad) requerida por el Cazador
-        Ability habilidad = new Will("Concentración", 1, 1, 1);
-        
-        // Construimos el personaje usando los métodos reales de CharacterBuilder
-        GameCharacter c = (GameCharacter) new HunterCharacterBuilder()
-                .withName("Cazador Novato")
-                .withBaseStats(5, 3) // Salud = 5, Poder = 3
-                .withAbility(habilidad)
-                .build();
->>>>>>> e396613e456f62282c76584337cdc7eaa7ab3bc2
 
         // ACT: Ejecutamos el algoritmo
         int result = instance.execute(c);
@@ -94,67 +78,4 @@ public class DamageAlgorithmTest {
         // ASSERT: Comprueba que el resultado es igual al poder base
         assertEquals("El daño debe ser igual al poder base si no hay armas ni modificadores", 3, result);
     }
-<<<<<<< HEAD
-=======
-
-    /**
-     * Escenario 2: Personaje con poder base + Arma equipada.
-     */
-    @Test
-    public void testExecute_WithWeapons() {
-        System.out.println("execute - Con Armas");
-        
-        // Instanciamos un arma con 2 puntos de ataque
-        Weapons arma = new Weapons("Espada Larga", 2, 0, 1); 
-        HashMap<String, Weapons> armasMap = new HashMap<>();
-        armasMap.put("Espada Larga", arma);
-        
-        Ability habilidad = new Will("Concentración", 1, 1, 1);
-        
-        // Construimos el personaje inyectándole el mapa de armas
-        GameCharacter c = (GameCharacter) new HunterCharacterBuilder()
-                .withName("Cazador Armado")
-                .withBaseStats(5, 4) // Poder base = 4
-                .withAbility(habilidad)
-                .withWeapons(armasMap)
-                .build();
-
-        // Ejecutamos el algoritmo
-        int result = instance.execute(c);
-        
-        // ASSERT: 4 (poder base) + 2 (ataque del arma) = 6
-        int expResult = 6; 
-        assertEquals("El daño debe ser la suma del poder base y el ataque del arma", expResult, result);
-    }
-
-    /**
-     * Escenario 3: Personaje con modificadores de Fortaleza.
-     */
-    @Test
-    public void testExecute_WithStrengths() {
-        System.out.println("execute - Con Modificadores (Fortalezas)");
-        
-        // Instanciamos una Fortaleza que sume 1 punto al modificador
-        Strength fortaleza = new Strength("Furia", 1); 
-        LinkedList<Strength> fortalezasList = new LinkedList<>();
-        fortalezasList.add(fortaleza);
-        
-        Ability habilidad = new Will("Concentración", 1, 1, 1);
-        
-        // Construimos el personaje inyectándole la lista de fortalezas
-        GameCharacter c = (GameCharacter) new HunterCharacterBuilder()
-                .withName("Cazador Fuerte")
-                .withBaseStats(5, 3) // Poder base = 3
-                .withAbility(habilidad)
-                .withStrengths(fortalezasList)
-                .build();
-
-        // Ejecutamos el algoritmo
-        int result = instance.execute(c);
-        
-        // ASSERT: 3 (poder base) + 1 (bonificador de fortaleza) = 4
-        int expResult = 4;
-        assertEquals("El daño debe incluir los modificadores activos de las fortalezas", expResult, result);
-    }
->>>>>>> e396613e456f62282c76584337cdc7eaa7ab3bc2
 }
